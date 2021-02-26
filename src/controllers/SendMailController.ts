e43b2653ec77dc0a596065ea82ede3afe47ad407
yuukiasuna00 <yuukixasuna00@gmail.com>
2021-02-26 17:40:41 -0300

    feat: Add custom error class known as AppError

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -6,6 +6,7 @@ import { SurveysRepository } from '../repositories/SurveysRepository';
 import { SurveysUsersRepository } from '../repositories/SurveysUsersRepository';
 import { UsersRepository } from '../repositories/UsersRepository';
 import SendMailService from '../services/SendMailService';
+import { AppError } from '../errors/AppError';
 
 class SendMailController {
   async execute(request: Request, response: Response) {
@@ -18,9 +19,7 @@ class SendMailController {
     const user = await usersRepository.findOne({ email });
 
     if (!user) {
-      return response.status(400).json({
-        error: 'User does not exists',
-      });
+      throw new AppError('User does not exists');
     }
 
     const survey = await surveysRepository.findOne({
@@ -28,9 +27,7 @@ class SendMailController {
     });
 
     if (!survey) {
-      return response.status(400).json({
-        error: 'Survey does not exists',
-      });
+      throw new AppError('Survey does not exists');
     }
 
     const npsPath = resolve(__dirname, '..', 'views', 'emails', 'npsMail.hbs');
